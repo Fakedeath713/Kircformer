948bd199203c3da2482e9e21aacd8634d91e963b
Fakedeath713 <azizcan.fb.7131@hotmail.com>
2024-04-16 01:35:17 +0300

    sfml cmake
    gcc version 13.2 required

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
-
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
 int main(int argc, char const *argv[])
 {
+    sf::VideoMode vm;
+    sf::RenderWindow* renderWindow;
+    vm.width  = 800;
+    vm.height = 600;
+    renderWindow = new sf::RenderWindow(vm, "kircformer", sf::Style::Titlebar | sf::Style::Close);
+
+    renderWindow->setFramerateLimit(60);
+    renderWindow->display();
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
